d89772c58a709ad0ae8287b6e395812a0e1b4cf4
marvelmiles <marvellousabidemi2@gmail.com>
2023-12-15 17:16:46 +0100

    mv-to-default

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@
 const isProdMode = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProdMode ? "/portfolio" : "",
-  assestPrefix: "/portfolio/",
-  output: "export",
-  distDir: "dist",
-  images: {
-    unoptimized: true
-  }
+  //   basePath: isProdMode ? "/portfolio" : "",
+  //   assestPrefix: "/portfolio/",
+  //   output: "export",
+  //   distDir: "dist",
+  //   images: {
+  //     unoptimized: true
+  //   }
 };
 
 module.exports = nextConfig;
