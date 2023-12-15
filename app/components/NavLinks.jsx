f407bc39b4eeaf145fabeae2905dfea58539d02f
marvelmiles <marvellousabidemi2@gmail.com>
2023-12-15 17:29:33 +0100

    mv-to-default

--- app/components/NavLinks.jsx
+++ app/components/NavLinks.jsx
@@ -80,6 +80,7 @@ const NavLinks = ({ className, itemClassName, width }) => {
           >
             <Link
               href={`/#${u.href}`}
+              as={`/#${u.href}`}
               data-to={u.href}
               style={{ width: "100%", display: "inline-block" }}
             >
