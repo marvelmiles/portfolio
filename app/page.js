adf7f9234a1425d6266582c9c30bf6796d16aaaa
Marvellous Abidemi AKinrinmola <marvellousabidemi2@gmail.com>
2024-01-08 23:36:11 +0100

    updated coding skill

--- app/page.js
+++ app/page.js
@@ -229,6 +229,8 @@ const Home = () => {
                     "Nextjs",
                     "Nodejs",
                     "MongoDB",
+                    "Agile Development",
+                    "Dev-Ops",
                   ]}
                 />
               </Fade>
