7956645b63abf5e2bb3afb4888d2e770ee4a9c3e
marvelmiles <marvellousabidemi2@gmail.com>
2024-09-12 20:50:03 +0100

    added works

--- app/components/Stats.jsx
+++ app/components/Stats.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import CountUp from "./CountUp";
 import Typography from "./Typography";
 import CountUp from "./CountUp";
+import { Fade } from "react-reveal";
 
 const Stats = () => {
   const stats = [
@@ -29,20 +29,22 @@ const Stats = () => {
   ];
 
   return (
-    <div className="flex items-center gap-4">
-      {stats.map((stat, i) => (
-        <div
-          className="
+    <Fade bottom>
+      <div className="flex items-center gap-4">
+        {stats.map((stat, i) => (
+          <div
+            className="
         flex items-center gap-3
         "
-        >
-          <CountUp to={stat.num} className={stat.className} />
-          <Typography className="font-bold max-w-[120px]">
-            {stat.text}
-          </Typography>
-        </div>
-      ))}
-    </div>
+          >
+            <CountUp to={stat.num} className={stat.className} />
+            <Typography className="font-bold max-w-[120px]">
+              {stat.text}
+            </Typography>
+          </div>
+        ))}
+      </div>
+    </Fade>
   );
 };
 
